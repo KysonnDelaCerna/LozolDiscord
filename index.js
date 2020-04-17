9bcff7a54e6b27b514cc4528356e92c2713f0b0b
KysonnDelaCerna <Kysonndelacerna@gmail.com>
2020-04-17 14:13:29 +0800

    new reacts

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ client.on('message', message => {
     if (message.content.toLowerCase().indexOf('suspension') !== -1 || message.content.toLowerCase().indexOf('suspend') !== -1) {
         message.reply('sorry no suspension :(((');
     }
-    if (message.content.indexOf('700390950832177242') !== -1 || message.content.toLowerCase().indexOf('lozol') !== -1 || message.content.toLowerCase().indexOf('la salle') !== -1 || message.content.toLowerCase().indexOf('dlsu') !== -1) {
+    if (message.content.indexOf('700390950832177242') !== -1 || message.content.toLowerCase().indexOf('lozol') !== -1 || message.content.toLowerCase().indexOf('la salle') !== -1 || message.content.toLowerCase().indexOf('dlsu') !== -1 || message.content.toLowerCase().indexOf('lasalle') !== -1) {
        message.react('699509158248185856');
+    }
+    if (message.content.toLowerCase().indexOf('up') !== -1 || message.content.toLowerCase().indexOf('ateneo') !== -1 || message.content.toLowerCase().indexOf('areneo') !== -1 || message.content.toLowerCase().indexOf('admu') !== -1 || message.content.toLowerCase().indexOf('ust') !== -1) {
+        message.react('🇬');
+        message.react('🇦');
+        message.react('🇾');
     }
 });
 
