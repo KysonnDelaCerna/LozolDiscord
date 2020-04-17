b462c9a286793afa83d8c21e417cec6af4ea5cb5
KysonnDelaCerna <Kysonndelacerna@gmail.com>
2020-04-17 17:23:56 +0800

    changed replies

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ client.on('message', message => {
     }
 
     if (message.content.toLowerCase().indexOf('refund') !== -1) {
-        message.reply('we will consider it');
+        message.channel.send(`> ${message.content}\n<@${message.author.id}> we will consider it`);
     }
     if (message.content.toLowerCase().indexOf('suspension') !== -1 || message.content.toLowerCase().indexOf('suspend') !== -1) {
-        message.reply('sorry no suspension :(((');
+        message.channel.send(`> ${message.content}\n<@${message.author.id}> sorry no suspension :(((`);
     }
     if (message.content.indexOf('700390950832177242') !== -1 || message.content.toLowerCase().indexOf('lozol') !== -1 || message.content.toLowerCase().indexOf('la salle') !== -1 || message.content.toLowerCase().indexOf('dlsu') !== -1 || message.content.toLowerCase().indexOf('lasalle') !== -1) {
        message.react('699509158248185856');
